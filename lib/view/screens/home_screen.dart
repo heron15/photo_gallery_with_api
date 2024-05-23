5a75a6110d7773f7b3786a728d850b539569a493
Heron <heronmoy1234@gmail.com>
2024-05-23 20:53:01 +0600

    change

--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:photo_gallery_with_api/controller/photo_data_controller.dart';
 import 'package:photo_gallery_with_api/services/photo_api_fetch.dart';
 import 'package:photo_gallery_with_api/utils/app_route.dart';
+import 'package:photo_gallery_with_api/utils/custom_color.dart';
 import 'package:photo_gallery_with_api/view/widgets/photo_list.dart';
 import '../../model/photo_model.dart';
 
@@ -62,24 +64,43 @@ class _HomeScreenState extends State<HomeScreen> {
                     ? const Center(
                         child: Text("No photos found."),
                       )
-                    : ListView.separated(
-                        itemCount: photoModel.length,
-                        itemBuilder: (context, index) {
-                          return PhotoList(
-                            photoUrl: photoModel[index].thumbnailUrl,
-                            photoTitle: photoModel[index].title,
-                            onTap: () {
-                              Navigator.pushNamed(
-                                context,
-                                AppRoute.detailsView,
-                                arguments: photoModel[index],
-                              );
-                            },
-                          );
-                        },
-                        separatorBuilder: (context, index) {
-                          return const Divider();
-                        },
+                    : Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                left: 15, top: 8, bottom: 5, right: 15),
+                            child: Text(
+                              "Total: ${photoModel.length} Item",
+                              style: const TextStyle(
+                                color: CustomColor.black,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: ListView.separated(
+                              itemCount: photoModel.length,
+                              itemBuilder: (context, index) {
+                                return PhotoList(
+                                  photoUrl: photoModel[index].thumbnailUrl,
+                                  photoTitle: photoModel[index].title,
+                                  onTap: () {
+                                    Navigator.pushNamed(
+                                      context,
+                                      AppRoute.detailsView,
+                                      arguments: photoModel[index],
+                                    );
+                                  },
+                                );
+                              },
+                              separatorBuilder: (context, index) {
+                                return const Divider();
+                              },
+                            ),
+                          ),
+                        ],
                       ),
               );
             }
